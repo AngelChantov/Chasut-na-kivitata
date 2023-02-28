0eaff4b0e5bd4f7edfdc73f951d3bd3900083dc0
Emil Korudzhiev <e.korudzhiev@abv.bg>
2023-02-28 23:46:34 +0200

    Project structure

--- src/main/java/game/kivita/chasutnakivitata/Controller.java
+++ src/main/java/game/kivita/chasutnakivitata/Controller.java
@@ -3,7 +3,7 @@ package game.kivita.chasutnakivitata;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Controller {
     @FXML
     private Label welcomeText;
 
